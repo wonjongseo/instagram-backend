14cee1e560357e146324ce677233f91826d16bc0
wonjongseo <visionwill@naver.com>
2022-03-02 15:32:34 +0900

    practice graphQl

--- server.js
+++ server.js
@@ -1,14 +1,46 @@
+import {PrismaClient} from "@prisma/client";
 import {ApolloServer, gql} from "apollo-server";
 
+const client = new PrismaClient();
+
 const typeDefs = gql`
+    type Movie {
+        id: Int!
+        title: String!
+        year: Int!
+        genre: String
+        createdAt: String!
+        updatedAt: String!
+    }
     type Query {
-        hello: String
+        movies: [Movie]
+        movie(id: Int!): Movie
+    }
+    type Mutation {
+        createMovie(title: String!, year: Int!, genre: String): Movie
+        deleteMovie(id: Int!): Movie
+        updateMovie(id: Int!, year: Int): Movie
     }
 `;
 
 const resolvers = {
     Query: {
-        hello: () => "bebe",
+        movies: () => client.movie.findMany(),
+        movie: (_, {id}) => client.movie.findUnique({where: id}),
+    },
+
+    Mutation: {
+        createMovie: (_, {title, year, genre}) =>
+            client.movie.create({
+                data: {
+                    title,
+                    year,
+                    genre,
+                },
+            }),
+        deleteMovie: (_, {id}) => client.movie.delete({where: id}),
+        updateMovie: (_, {id, year}) =>
+            client.movie.update({where: id, data: {year}}),
     },
 };
 
@@ -20,14 +52,3 @@ const server = new ApolloServer({
 server
     .listen()
     .then(() => console.log("Server is running on http://localhost:4000/"));
-
-/*
-    npm i apollo-server graphql
-npm i nodemon --save-dev
-
-in  babel.config.json
-
-{
-  "presets": ["@babel/preset-env"]
-}
-*/
