b039af45a19786224409a8f6da74dcad49660c33
wonjongseo <visionwill@naver.com>
2022-03-14 20:29:24 +0900

    see feec

--- src/photos/seeFeed/seeFeed.resolvers.js
+++ src/photos/seeFeed/seeFeed.resolvers.js
@@ -3,28 +3,31 @@ import {protectResolver} from "../../users/users.utils";
 
 export default {
     Query: {
-        seeFeed: protectResolver((_, __, {loggedInUser}) =>
-            client.photo.findMany({
-                // where: {
-                //     OR: [
-                //         {
-                //             user: {
-                //                 followers: {
-                //                     some: {
-                //                         id: loggedInUser.id,
-                //                     },
-                //                 },
-                //             },
-                //         },
-                //         {
-                //             userId: loggedInUser.id,
-                //         },
-                //     ],
-                // },
+        seeFeed: (_, __, {loggedInUser}) => client.photo.findMany(),
+    },
+};
+
+/*
+{
+                where: {
+                    
+                    // OR: [
+                    //     {
+                    //         user: {
+                    //             followers: {
+                    //                 some: {
+                    //                     id: loggedInUser.id,
+                    //                 },
+                    //             },
+                    //         },
+                    //     },
+                    //     {
+                    //         userId: loggedInUser.id,
+                    //     },
+                    // ],
+                },
                 orderBy: {
                     createAt: "desc",
                 },
-            })
-        ),
-    },
-};
+            }
+*/
